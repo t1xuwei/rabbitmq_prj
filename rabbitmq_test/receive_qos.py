63785c231eb3576e1e5c80d85b83f4ca6ed5051e
xuw <xuw@pingansec.com>
2017-09-07 17:16:21 +0800

    test

--- rabbitmq_test/receive_qos.py
+++ rabbitmq_test/receive_qos.py
@@ -16,7 +16,7 @@ print ' [*] Waiting for messages. To exit press CTRL+C'
   
 def callback(ch, method, properties, body):  
     print " [x] Received: %r!!" % (body,)
-    time.sleep(3)
+    time.sleep(1)
 
 channel.basic_qos(1)
 # no_ack为false时，每次启动接收脚本都会收到队列里面的信息

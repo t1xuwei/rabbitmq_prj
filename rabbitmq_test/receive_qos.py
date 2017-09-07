f716075c996423fef21df2b4aa293a0f1344c631
xuw <xuw@pingansec.com>
2017-09-07 17:25:09 +0800

    test

--- rabbitmq_test/receive_qos.py
+++ rabbitmq_test/receive_qos.py
@@ -18,7 +18,7 @@ def callback(ch, method, properties, body):
     print " [x] Received: %r!!" % (body,)
     time.sleep(1)
 
-channel.basic_qos(1)
+channel.basic_qos(prefetch_count=1)
 # no_ack为false时，每次启动接收脚本都会收到队列里面的信息
 channel.basic_consume(callback,
                       queue='hello',
